ba8b6f3b13ba37e5a2e3351221c692dbd193daa7
Edmundo Torres-Gonzalez <edmundotogo@gmail.com>
2024-12-18 09:05:38 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add functionality to the button
+document.getElementById("myButton").addEventListener("click", function() {
+  document.getElementById("message").textContent = "Hello! You clicked the button.";
+});
+
