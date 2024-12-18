a28489983a45d5e6cafaa96a3e2e84372d6e0cd6
Edmundo Torres-Gonzalez <edmundotogo@gmail.com>
2024-12-18 10:03:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ toggleButton.addEventListener('click', () => {
     message.style.display = 'none'; // Hide the message
   }
 });
+
+
+// Fetch the content from the about.txt file
+fetch('about.txt')
+  .then(response => response.text()) // Get the text content
+  .then(data => {
+    // Insert the text into the #about-text div
+    document.getElementById('about-text').innerText = data;
+  })
+  .catch(error => console.error('Error fetching About Me content:', error));
+
